7b837fc5769e6ad72ed81d57260a9d5c41256ee2
Wangui Kamande <kamande@leicunnstudio.com>
2019-12-24 12:53:04 +0300

    Read page updated footer. Index page web optimized footer

--- js/index.js
+++ js/index.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        gutter: 40,
-        columnWidth: '.grid-sizer',
-        percentPosition: true,
-        horizontalOrder: true,
-        resize: true,
-        fitWidth: true
-    });
+    // $('.modal').modal();
 
 });
